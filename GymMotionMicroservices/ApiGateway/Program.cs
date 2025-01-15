dd05901244843eb7c9e10b85bb2e0984b080f604
JowyYo <jodommad@telefonica.net>
2025-01-15 21:25:07 +0100

    Api gateway in progress

---
+++ GymMotionMicroservices/ApiGateway/Program.cs
@@ -0,0 +1,26 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Configuration.AddJsonFile("ocelot.json");
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseOcelot().Wait();
+
+app.UseHttpsRedirection();
+
+app.Run();
